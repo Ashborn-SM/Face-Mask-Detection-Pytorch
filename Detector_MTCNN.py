3fd4542a8cd46ba6ff9ee90b4ebbcf638e917e7c
Rahul0128 <66192267+Rahul0128@users.noreply.github.com>
2020-07-25 14:46:16 +0530

    Update Detector_MTCNN.py

--- Detector_MTCNN.py
+++ Detector_MTCNN.py
@@ -54,7 +54,7 @@ while True:
         # if the video is too big uncomment the below code
         #frame = resize(frame, height, width)
         
-        #padding the image to makesure the bounding goes out of the image
+        #padding the image to avoid the bounding going out of the image
         #and crashes the program
         padding =  cv.copyMakeBorder(frame, 50,50,50,50, cv.BORDER_CONSTANT)
         #converting numpy array into image
